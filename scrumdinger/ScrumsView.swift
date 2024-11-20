18a64a44ee996b162e39b96c316b084576375846
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2024-11-20 12:06:32 +0700

    feat: scrums list view

---
+++ scrumdinger/ScrumsView.swift
@@ -0,0 +1,23 @@
+//
+//  ScrumsView.swift
+//  scrumdinger
+//
+//  Created by Danil Hendra Suryawan on 20/11/24.
+//
+
+import SwiftUI
+
+struct ScrumsView : View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
