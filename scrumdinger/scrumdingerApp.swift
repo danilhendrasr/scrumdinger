9a17fac4151a7046d21bdcbbc254d3e1cc4dc9d4
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2024-11-20 08:34:48 +0700

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Danil Hendra Suryawan on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
---
+++ scrumdingerTests/scrumdingerTests.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerTests.swift
+//  scrumdingerTests
+//
+//  Created by Danil Hendra Suryawan on 20/11/24.
+//
+
+import Testing
+@testable import scrumdinger
+
+struct scrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
