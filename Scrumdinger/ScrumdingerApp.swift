89a5bbc103e90cfce6dfb12b07fd94496a0db681
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2024-11-27 07:11:55 +0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Danil Hendra Suryawan on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
